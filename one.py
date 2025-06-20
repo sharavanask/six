79628a9bc8518cc8d99ed5959cf09bc7423f157e
Sharavana Kumar S <132196939+sharavanask@users.noreply.github.com>
2025-06-20 14:55:14 +0530

    Create one.py

---
+++ one.py
@@ -0,0 +1,23 @@
+# Buggy Python Script
+def calculate_avg(numbers)
+    total = 0
+    for i in range(len(numbers)):
+        total += numbers[i
+    avg = total / len(numberz)
+    return avg
+
+def print_greeting(name):
+    if name = "":
+        print("Hello, Stranger"
+    else
+        print("Hello" + name)
+
+def main()
+    nums = [10, 20, "thirty", 40, 50]
+    average = calculate_avg(nums)
+    print("The average is: " + average)
+
+    name = input("Enter your name: )
+    print_greeting(name)
+
+main
